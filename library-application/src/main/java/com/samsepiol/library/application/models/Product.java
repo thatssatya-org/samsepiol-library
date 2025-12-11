0ab1fa9fd845fa3295c60b3ba9e63186e028897b
Satyajit <thatssatya@gmail.com>
2025-12-12 01:06:13 +0530

    BsonCreator added on entities' all args constructor

--- library-application/src/main/java/com/samsepiol/library/application/models/Product.java
+++ library-application/src/main/java/com/samsepiol/library/application/models/Product.java
@@ -1,23 +1,30 @@
 package com.samsepiol.library.application.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.samsepiol.library.mongo.models.Entity;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.ToString;
+import lombok.Value;
 import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
+import org.bson.codecs.pojo.annotations.BsonCreator;
+import org.bson.codecs.pojo.annotations.BsonProperty;
 
-@Data
+@Value
+@Jacksonized
 @SuperBuilder
-@NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(onConstructor_ = {@BsonCreator})
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class Product extends Entity {
     private static final String PREFIX = "P";
 
+    @BsonProperty("name")
     String name;
 
     @Override
--- message-queue/kafka/src/main/java/com/samsepiol/library/kafka/consumer/config/service/repo/models/ConsumerConfigEntity.java
+++ message-queue/kafka/src/main/java/com/samsepiol/library/kafka/consumer/config/service/repo/models/ConsumerConfigEntity.java
@@ -1,37 +1,45 @@
 package com.samsepiol.library.kafka.consumer.config.service.repo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.samsepiol.library.kafka.consumer.config.service.repo.models.enums.ConsumerStatus;
 import com.samsepiol.library.mongo.models.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.Value;
 import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
+import org.bson.codecs.pojo.annotations.BsonCreator;
 import org.bson.codecs.pojo.annotations.BsonProperty;
 
-@Data
+@Value
 @SuperBuilder
-@NoArgsConstructor
-@AllArgsConstructor
+@Jacksonized
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor(onConstructor_ = {@BsonCreator})
 public class ConsumerConfigEntity extends Entity {
     private static final String ID_PREFIX = "CE";
     public static final String STATUS_KEY = "status";
 
     @NonNull
-    private String topic;
+    @BsonProperty("topic")
+    String topic;
 
     @NonNull
-    private String messageHandler;
+    @BsonProperty("messageHandler")
+    String messageHandler;
 
     @NonNull
     @Builder.Default
-    private Integer concurrency = 1;
+    @BsonProperty("concurrency")
+    Integer concurrency = 1;
 
     @NonNull
     @Builder.Default
     @BsonProperty(STATUS_KEY)
-    private ConsumerStatus status = ConsumerStatus.INACTIVE;
+    ConsumerStatus status = ConsumerStatus.INACTIVE;
 
     @Override
     protected @NonNull String getIdPrefix() {
