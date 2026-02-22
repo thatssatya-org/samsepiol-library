d20964e1465cabb42214e6ec18449666576369dc
Satyajit <thatssatya@gmail.com>
2026-02-22 05:57:41 +0530

    TemporalWorkflow graceful start method exposed from interface

--- temporal/src/main/java/com/samsepiol/library/temporal/workflow/TemporalWorkflow.java
+++ temporal/src/main/java/com/samsepiol/library/temporal/workflow/TemporalWorkflow.java
@@ -1,4 +1,21 @@
 package com.samsepiol.library.temporal.workflow;
 
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowExecutionAlreadyStarted;
+import io.temporal.workflow.Functions;
+
+import java.util.logging.Logger;
+
 public interface TemporalWorkflow {
+    Logger LOGGER = Logger.getLogger(TemporalWorkflow.class.getName());
+
+    static <T> boolean startGracefully(Functions.Proc1<T> workflow, T request) {
+        try {
+            WorkflowClient.start(workflow, request);
+            return Boolean.TRUE;
+        } catch (WorkflowExecutionAlreadyStarted exception) {
+            LOGGER.info(String.format("Workflow execution already started for request: %s", request.toString()));
+        }
+        return Boolean.FALSE;
+    }
 }
